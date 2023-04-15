3568f4a14f4a2ad0113a2cece713aad9292c5b22
Natanael <69061503+NatanaelAlexander@users.noreply.github.com>
2023-04-15 17:37:31 -0400

    Se realizo la maquetación + CSS
    Se realizo maquetación más slider, ahora me falta el JS y sigo con el curso.

--- Proyecto 1 JQUERY/js/main.js
+++ Proyecto 1 JQUERY/js/main.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", () => {
-
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: false,
+        stopAutoOnClick: false,
+        pager: false,
+        slideWidth: 1000
+    });
 }); 
