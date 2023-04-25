87f5946f932bca5f7d26b52c8be81d6e7c7616e5
Natanael <69061503+NatanaelAlexander@users.noreply.github.com>
2023-04-25 17:34:34 -0400

    AGREGO JS PARA POSTS DESDE JS
    se borro los posts desde html y se agrego desde js, mediante un json

--- Proyecto 1 JQUERY/js/main.js
+++ Proyecto 1 JQUERY/js/main.js
@@ -6,4 +6,56 @@ window.addEventListener("load", () => {
         pager: false,
         slideWidth: 1000
     });
+    //post
+    var posts = [
+        {
+            title: 'prueba de titlo',
+            date: 'Fecha de publicación: ' + moment().format("MMM dddd Do YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu lacus in tortor pretiume fficitur.Cras id rhoncus magna.In et mollis ex.Donec tempus lectus sed quam aliquam, euismod dictum sapien vehicula.Proin semper ipsum a magna ultricies faucibus.In hac habitasse plateadictumst.Vivamus id nibh non dui euismod tincidunt vitae sit amet nisi.Duis sagittis nislvitae risus ornare, at condimentum ligula ultricies.Praesent malesuada eget lectus idfacilisis.Etiam lorem orci, suscipit sit amet volutpat in, aliquam non purus.Proin congue,lectus at sodales euismod, ipsum magna accumsan nisi, dignissim tincidunt orci augue eu dui.Suspendisse sapien nisi, porta hendrerit ultrices nec, vehicula quis sem.Praesent massa dui,aliquam et lorem eu, egestas facilisis od'
+        },
+        {
+            title: 'prueba de titlo',
+            date: new Date(),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu lacus in tortor pretiume fficitur.Cras id rhoncus magna.In et mollis ex.Donec tempus lectus sed quam aliquam, euismod dictum sapien vehicula.Proin semper ipsum a magna ultricies faucibus.In hac habitasse plateadictumst.Vivamus id nibh non dui euismod tincidunt vitae sit amet nisi.Duis sagittis nislvitae risus ornare, at condimentum ligula ultricies.Praesent malesuada eget lectus idfacilisis.Etiam lorem orci, suscipit sit amet volutpat in, aliquam non purus.Proin congue,lectus at sodales euismod, ipsum magna accumsan nisi, dignissim tincidunt orci augue eu dui.Suspendisse sapien nisi, porta hendrerit ultrices nec, vehicula quis sem.Praesent massa dui,aliquam et lorem eu, egestas facilisis od'
+        },
+        {
+            title: 'prueba de titlo',
+            date: new Date(),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu lacus in tortor pretiume fficitur.Cras id rhoncus magna.In et mollis ex.Donec tempus lectus sed quam aliquam, euismod dictum sapien vehicula.Proin semper ipsum a magna ultricies faucibus.In hac habitasse plateadictumst.Vivamus id nibh non dui euismod tincidunt vitae sit amet nisi.Duis sagittis nislvitae risus ornare, at condimentum ligula ultricies.Praesent malesuada eget lectus idfacilisis.Etiam lorem orci, suscipit sit amet volutpat in, aliquam non purus.Proin congue,lectus at sodales euismod, ipsum magna accumsan nisi, dignissim tincidunt orci augue eu dui.Suspendisse sapien nisi, porta hendrerit ultrices nec, vehicula quis sem.Praesent massa dui,aliquam et lorem eu, egestas facilisis od'
+        },
+        {
+            title: 'prueba de titlo',
+            date: new Date(),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu lacus in tortor pretiume fficitur.Cras id rhoncus magna.In et mollis ex.Donec tempus lectus sed quam aliquam, euismod dictum sapien vehicula.Proin semper ipsum a magna ultricies faucibus.In hac habitasse plateadictumst.Vivamus id nibh non dui euismod tincidunt vitae sit amet nisi.Duis sagittis nislvitae risus ornare, at condimentum ligula ultricies.Praesent malesuada eget lectus idfacilisis.Etiam lorem orci, suscipit sit amet volutpat in, aliquam non purus.Proin congue,lectus at sodales euismod, ipsum magna accumsan nisi, dignissim tincidunt orci augue eu dui.Suspendisse sapien nisi, porta hendrerit ultrices nec, vehicula quis sem.Praesent massa dui,aliquam et lorem eu, egestas facilisis od'
+        }, {
+            title: 'prueba de titlo',
+            date: new Date(),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu lacus in tortor pretiume fficitur.Cras id rhoncus magna.In et mollis ex.Donec tempus lectus sed quam aliquam, euismod dictum sapien vehicula.Proin semper ipsum a magna ultricies faucibus.In hac habitasse plateadictumst.Vivamus id nibh non dui euismod tincidunt vitae sit amet nisi.Duis sagittis nislvitae risus ornare, at condimentum ligula ultricies.Praesent malesuada eget lectus idfacilisis.Etiam lorem orci, suscipit sit amet volutpat in, aliquam non purus.Proin congue,lectus at sodales euismod, ipsum magna accumsan nisi, dignissim tincidunt orci augue eu dui.Suspendisse sapien nisi, porta hendrerit ultrices nec, vehicula quis sem.Praesent massa dui,aliquam et lorem eu, egestas facilisis od'
+        },
+    ];
+
+    posts.forEach((item, index) => {
+        var post = `
+
+        <article class="post">
+        
+        <h2>${item.title + " " + (index + 1)}</h2>
+        
+        <span class="date">${item.date}</span>
+        
+        <p>
+        
+        ${item.content}
+        
+        </p>
+        
+        <a href="#" class="button-more">Leer más</a>
+        
+        </article>
+        
+        `;
+        $("#posts").append(post);
+    });
+
+
 }); 
